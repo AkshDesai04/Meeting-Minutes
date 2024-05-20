b402c694063c75411fc5ee96dd3c79563f751fa6
Aksh Desai <aksh.d.4002@gmail.com>
2024-05-20 10:29:40 +0530

    Added temp static path to input file

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import ingest
 import data_cleanup
 
 def main():
-    file_path = input("Enter file path")
+    # file_path = input("Enter file path")
+    file_path = 'test.txt'
     file_content = ingest.ingest_target(file_path)
     file_content = data_cleanup.master_cleanup(file_content)
     print(file_content)
