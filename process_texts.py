e87718f409ca68be84fa5339f962b2fb51ba045d
Aksh Desai <aksh.d.4002@gmail.com>
2024-05-20 12:11:07 +0530

    Minor Changes in main

--- process_texts.py
+++ process_texts.py
@@ -14,14 +14,17 @@ def process_texts(list):
 
         
 def process_texts_single_prompt(text):
-    print("Statement: " + str(text['text']))
     prompt_summarize = "Summarize this sentence for me: " + str(text['text'])
-    print("Summary: " + summary)
+    print(prompt_summarize)
     prompt_categorize = "Is this sentence a \'Information\' \'Decision\', or \'Action Item\': " + str(text['text'])
-    print("Category: " + category)
+    print(prompt_categorize)
 
-    summary = prompter.prompt(prompt_summarize)
-    category = prompter.prompt(prompt_categorize)
+    # summary = prompter.prompt(prompt_summarize)
+    # category = prompter.prompt(prompt_categorize)
 
-    print(summary)
-    print(category)
+    # print(summary)
+    # print(category)
+    
+    # print("Statement: " + str(text['text']))
+    # print("Summary: " + summary)
+    # print("Category: " + category)
