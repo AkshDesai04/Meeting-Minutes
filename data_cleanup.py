5827fa0abb9097c4e463e222ac4d1ca47f9d7919
Aksh Desai <aksh.d.4002@gmail.com>
2024-05-20 09:08:12 +0530

    Improved manual filteration

--- data_cleanup.py
+++ data_cleanup.py
@@ -13,7 +13,6 @@ def master_cleanup(list):
     return list
 
 def manual_cleanup(list, word_count = 5):
-
     filler_words = ['um', 'uh', 'er', 'like', 'well', 'you know', 'so', 'anyway', 'i mean', 'actually', 'basically', 'literally', 'kind of', 'sort of', 'right', 'exactly', 'right', 'yeah', 'mhmm', 'hmm', 'thank you', 'thank you so much', 'thank you very much']
     list = remove_small_sentences(list, word_count)
     list = remove_filler(list, filler_words, 0.5)
@@ -23,16 +22,23 @@ def remove_small_sentences(list, word_count):
     list = [item for item in list if len(item['text'].split()) >= word_count]
     return list
 
-def remove_filler(text_items, fillers, filler_percentage=0.5):
-    filtered_items = []
-    for item in text_items:
-        text_words = item['text'].lower().split()  # Convert text to lowercase before splitting
-        contains_filler = any(filler.lower() in text_words for filler in fillers)  # Convert fillers to lowercase for comparison
-        text_length = len(text_words)
-        filler_threshold = text_length * (1 - filler_percentage)
-        if not contains_filler and text_length >= filler_threshold:
-            filtered_items.append(item)
-    return filtered_items
+def remove_filler(list, fillers, filler_percentage=0.5):
+    filtered_list = []
+    
+    for text in list:
+        original_length = len(text)
+        modified_text = text
+        
+        for filler in fillers:
+            modified_text = modified_text.replace(filler, '')
+        
+        modified_length = len(modified_text)
+        
+        if modified_length/original_length >= filler_percentage:
+            filtered_list.append(text)
+    
+    list[:] = filtered_list
+    return list
 
 def prompted_cleanup(list, threshold=0.8):
     for item in list:
