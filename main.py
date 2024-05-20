e454405278920ac1331bd825adf0727be38fd169
Aksh Desai <aksh.d.4002@gmail.com>
2024-05-20 16:40:48 +0530

    Minor Changes in main

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import data_cleanup
 import process_texts
 
 def main():
-    # file_path = input("Enter file path")
-    # file_path = 'test.txt'
-    file_path = './Meeting-Minutes/test.txt'
+    # file_path = input("Enter file path") # Use this for user input
+    # file_path = 'test.txt' # Use this for on device testing
+    file_path = './Meeting-Minutes/test.txt' # Use this for Google Colab testing
     file_content = ingest.ingest_target(file_path)
     file_content = data_cleanup.master_cleanup(file_content)
     print(file_content)
