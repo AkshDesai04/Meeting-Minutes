c1f04acc1faca003a70df13d6a720424fad689af
Aksh Desai <aksh.d.4002@gmail.com>
2024-05-20 10:34:44 +0530

    Minor Changes in main

---
+++ git.bat
@@ -0,0 +1,3 @@
+git add ./
+git commit -m "Minor Changes in main"
+git push
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import process_texts
 
 def main():
     # file_path = input("Enter file path")
-    file_path = 'test.txt'
+    file_path = './test.txt'
     file_content = ingest.ingest_target(file_path)
     file_content = data_cleanup.master_cleanup(file_content)
     print(file_content)
