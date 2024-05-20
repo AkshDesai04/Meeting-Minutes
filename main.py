cea140382ff8dee742984481a2ad6f96bed1ce60
Aksh Desai <aksh.d.4002@gmail.com>
2024-05-20 10:55:23 +0530

    Minor Changes in main

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import process_texts
 
 def main():
     # file_path = input("Enter file path")
-    file_path = 'test.txt'
+    # file_path = 'test.txt'
+    file_path = './Meeting-Minutes/test.txt'
     file_content = ingest.ingest_target(file_path)
     file_content = data_cleanup.master_cleanup(file_content)
     print(file_content)
