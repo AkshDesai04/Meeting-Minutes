84fff17b3484a5a8808d3e6025fee6a80c73185b
Aksh Desai <aksh.d.4002@gmail.com>
2024-05-21 10:52:56 +0530

    Added Temp

--- process_texts.py
+++ process_texts.py
@@ -5,9 +5,24 @@ def process_texts(list):
         prompt_summarize = "Summarize this sentence for me: " + text['text']
         prompt_categorize = "Is this sentence a \'Information\' \'Decision\', or \'Action Item\': " + text['text']
 
-        # summary = prompter.prompt(prompt_summarize)
-        # category = prompter.prompt(prompt_categorize)
+        summary = prompter.prompt(prompt_summarize)
+        category = prompter.prompt(prompt_categorize)
 
-        # print("Statement: " + text['text'])
-        # print("Summary: " + summary)
-        # print("Category: " + category)
+        print("Statement: " + text['text'])
+        print("Summary: " + summary)
+        print("Category: " + category)
+
+        
+def process_texts_single_prompt(text):
+    print("Statement: " + text)
+    prompt_summarize = "Summarize this statement in less with 10 words: " + str(text)
+    prompt_categorize = "Reply in one word only. Is this statement a \'Information\' \'Decision\', or \'Action Item\': " + str(text)
+
+    # summary = prompter.prompt(prompt_summarize)
+    # print("summary: " + summary)
+    # category = prompter.prompt(prompt_categorize)
+    # print("category: " + category)
+    
+    print()
+    print()
+    print()
